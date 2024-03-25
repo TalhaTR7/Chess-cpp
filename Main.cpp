3842d7182bcc28bb1bccd3941b82b47683d841a8
TalhaTR7 <www.talharafat@gmail.com>
2024-03-25 09:00:30 +0500

    Changed alot

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
 
+#define dimension 8
+char** Board;
+
+struct position {
+    int row, col;
+    position (int x = 0, int y = 0)
+        :row(x),  col(y) {}
+};
+
+
+void initialize() {
+    Board = new char* [dimension];
+    for (int i = 0; i < dimension; i++) {
+        Board[i] = new char [dimension];
+        for (int y = 0; y < dimension; y++) {
+            Board[i][y] = '.';
+        }
+    }
+}
+
+void printBoard() {
+    char label1 = 'A', label2 = 'H';
+    cout << "   1 2 3 4 5 6 7 8" << endl << endl;
+    for (int i = 0; i < dimension; i++) {
+        cout << label1++ << ' ' << ' ';
+        for (int j = 0; j < dimension; j++) {
+            cout << Board[i][j] << ' ';
+        }
+        cout << ' ' << label2-- << endl;
+    }
+    cout << endl << "   8 7 6 5 4 3 2 1" << endl;
+}
+
 int main() {
-    cout << "Hell World";
-    return 0;
+    
+    // Initialize the board.
+    initialize();
+    
+    // Print initial state of the board.
+    printBoard();
 }
