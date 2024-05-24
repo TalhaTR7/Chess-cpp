af390ea73c6a129d3d06f6fbb09e25398f598619
TalhaTR7 <www.talharafat@gmail.com>
2024-05-24 20:02:39 +0500

    changed the whole project

--- Main.cpp
+++ Main.cpp
@@ -1,88 +1,59 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-#define dimension 8
-enum class player { WHITE, BLACK };
-player turn = player::WHITE;
-char** Board = nullptr;
+class card
+{
+private:
 
-void initialize() {
-    Board = new char* [dimension];
-    for (int i = 0; i < dimension; i++) {
-        Board[i] = new char [dimension];
-        for (int j = 0; j < dimension; j++) {
-            if (i == 0 and j == 0 or i == 0 and j == 7) Board[i][j] = 'R';
-            else if (i == 0 and j == 1 or i == 0 and j == 6) Board[i][j] = 'H';
-            else if (i == 0 and j == 2 or i == 0 and j == 5) Board[i][j] = 'B';
-            else if (i == 7 and j == 0 or i == 7 and j == 7) Board[i][j] = 'r';
-            else if (i == 7 and j == 1 or i == 7 and j == 6) Board[i][j] = 'h';
-            else if (i == 7 and j == 2 or i == 7 and j == 5) Board[i][j] = 'b';
-            else if (i == 1) Board[i][j] = 'P';
-            else if (i == 0 and j == 3) Board[i][j] = 'K';
-            else if (i == 0 and j == 4) Board[i][j] = 'Q';
-            else if (i == 6) Board[i][j] = 'p';
-            else if (i == 7 and j == 3) Board[i][j] = 'k';
-            else if (i == 7 and j == 4) Board[i][j] = 'q';
-            else Board[i][j] = '.';
+    int cardID;
+    int value;
+    char suit;
+
+public:
+
+    card(int a = 0, int b = 0, char c = 0)
+        :cardID(a), value(b), suit(c) {}
+
+    friend ostream& operator<< (ostream& out, card& myCard) {
+        string V;
+        char S;
+        if (myCard.value == 1) V = "A";
+        else if (myCard.value == 11) V = "J";
+        else if (myCard.value == 12) V = "Q";
+        else if (myCard.value == 13) V = "K";
+        else V = to_string(myCard.value);
+
+        if (myCard.suit == 'H') S = char(3);
+        else if (myCard.suit == 'D') S = char(4);
+        else if (myCard.suit == 'C') S = char(5);
+        else S = char(6);
+
+        out << V << S;
+        return out;
+    }
+};
+
+/*
+card* deck = nullptr;
+
+void fillDeck() {
+    string value[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    int cardID = 0;
+    deck = new card[52];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 13; j++) {
+            if (i == 0) deck[i] = card(cardID++, value[j], "Diamond");
+            else if (i == 1) deck[i] = card(cardID++, value[j], "Clubs");
+            else if (i == 2) deck[i] = card(cardID++, value[j], "Hearts");
+            else deck[i] = card(cardID++, value[j], "Spades");
         }
     }
 }
-
-char givePiece(int row, int col) {
-    return Board[row][col];
-}
-
-
-void switchTurn() {
-    if (turn == player::BLACK) turn = player::WHITE;
-    else turn = player::BLACK;
-}
-
-void printBoard() {
-    cout << endl;
-    for (int i = 0; i < dimension; i++) {
-        for (int j = 0; j < dimension; j++)
-            cout << Board[i][j] << ' ';
-        cout << endl;
-    }
-}
-
-bool verifyBox(char boxRow, int boxCol) {
-    if (toupper(boxRow) >= 'A' and toupper(boxRow) <= 'H')
-        if (boxCol >= 1  and boxCol <= 8)
-            return true;
-    return false;
-}
-
-bool isValidBox(int row, int col) {
-    if (turn == player::WHITE) {
-        return (givePiece(row, col) == 'R' or
-            givePiece(row, col) == 'H' or
-            givePiece(row, col) == 'B' or
-            givePiece(row, col) == 'K' or
-            givePiece(row, col) == 'Q' or
-            givePiece(row, col) == 'P')
-            ? true : false;
-    }
-    else if (turn == player::BLACK) {
-        return (givePiece(row, col) == 'r' or
-            givePiece(row, col) == 'h' or
-            givePiece(row, col) == 'b' or
-            givePiece(row, col) == 'k' or
-            givePiece(row, col) == 'q' or
-            givePiece(row, col) == 'p')
-            ? true : false;
-    }
-    return false;
-}
-
-void makeMove() {
-}
+*/
 
 int main() {
-    char row;
-    int col;
-    cout << "enter: ";
-    cin >> row >> col;
-    cout << verifyBox(row, col);
+    card D5 = card(1, 10, 'S');
+    cout << D5;
 }
